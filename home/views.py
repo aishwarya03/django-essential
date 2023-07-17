55fac0cc5da43349c31b010c24b66259a3e50857
Aishwarya <aishwaryaa1@hotmail.com>
2023-07-18 02:40:50 +0530

    static files essential

--- home/views.py
+++ home/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
-    extra_content =  {'today': datetime.today()}
+    extra_context = {'today': datetime.today()}
 
 
 class AuthorizedView(LoginRequiredMixin, TemplateView):
